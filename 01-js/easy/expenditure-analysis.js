7e0578b2bb0f403b74ce0c4599ea2439812bb500
biswa418 <biswajeetsutar418@gmail.com>
2023-06-12 19:22:56 +0530

    expenditure problem - solved

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,55 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  //ans list
+  let ans = {};
+  let ansList = [];
+
+  transactions.map((transaction) => {
+    const { category, price } = transaction;
+
+    if (ans[category]) {
+      ans[category] += price;
+    } else {
+      ans[category] = price;
+    }
+  });
+
+  for (let i in ans) {
+    ansList.push({ [i]: ans[i] })
+  }
+
+  return ansList;
 }
+
+// let ans = calculateTotalSpentByCategory([
+//   {
+//     itemName: "Soap",
+//     category: "daily",
+//     price: 35,
+//     timestamp: '21-01-2023'
+//   },
+//   {
+//     itemName: "Phone",
+//     category: "electronics",
+//     price: 350,
+//     timestamp: '21-01-2023'
+//   },
+//   {
+//     itemName: "Charger",
+//     category: "electronics",
+//     price: 100,
+//     timestamp: '21-01-2023'
+//   },
+
+//   {
+//     itemName: "Masala",
+//     category: "daily",
+//     price: 50,
+//     timestamp: '21-01-2023'
+//   },
+// ]);
+
+// console.log(ans);
 
 module.exports = calculateTotalSpentByCategory;
